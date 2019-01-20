27160b62e14e653599fdffc517bc8e8c19549ea2
Salman Ahmad <salman.neim@gmail.com>
2019-01-20 23:00:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,18 @@ end
 
 def summon_captain_planet# code an argument here
   # Your code here
-  
+   nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
